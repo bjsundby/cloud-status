177d357eeb6bf90b6fb0a0d26ecbbe02dcd2c1cc
Bjørn Sundby <bjorn.sundby@technipfmc.com>
2017-02-23 12:43:58 +0100

    Removed standard content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import {getFlagPosition} from './Service';
 
 import './App.css';
@@ -27,8 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Build Flag</h2>
         </div>
         <p className="App-intro">
           Flag position {this.state.flagPosition.position} as you see.
