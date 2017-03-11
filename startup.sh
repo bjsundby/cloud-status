939bddcad63e71b6d946e27a41b33eefe122ba6c
bjsundby@online.no <bjsundby@online.no>
2017-03-11 17:49:57 +0100

    Updated startup

--- startup.sh
+++ startup.sh
@@ -3,9 +3,8 @@
 #
 # Edit crontab file by running crontab -e
 # Add the following line refering to this file's position
-#  @reboot sh /home/pi/build-flag/startup.sh
+#  @reboot /usr/bin/sudo -u pi -H sh /home/pi/share/build-flag/startup.sh
 #
 
 cd /home/pi/share/build-flag
 sudo npm start
-
