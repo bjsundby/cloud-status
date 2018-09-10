0f811fd5824aff41ad41c54aab6ea872111bd7d4
Bjørn Sundby <bjsundby@online.no>
2018-09-10 14:57:11 +0200

    Renamed current led function

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var ledFunction = {
 
 const numberOfLeds = 15
 
-var currentLedFunction = ledFunction.Blink
+var currentLedFunction = ledFunction.BLINK
 
 var ledBlinkState = false
 var ledCurrent = 0
